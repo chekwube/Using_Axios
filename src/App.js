8409388e37f388ef2c0018f21eb22d9fd4b006db
cheks <okekechekwube1@gmail.com>
2018-11-13 12:12:55 +0100

    Consuming an API using axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      username: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+  //console.log('Success!')
+  axios.get('https://api.github.com/users/chekwube')
+    .then(response => this.setState({
+      username: response.data.name
+    }))
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>Click me</button>
+        <p>{this.state.username}</p>
       </div>
     );
   }
